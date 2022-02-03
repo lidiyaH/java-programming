69a68dc5cd1f85b979d6820b29dcdaf81f5cda61
lidiya <lidiyamuna@gmail.com>
2022-02-02 19:26:05 -0600

    anim

---
+++ src/day44_custom_classes/AnimalObjects.java
@@ -0,0 +1,18 @@
+package day44_custom_classes;
+
+public class AnimalObjects {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        System.out.println(animal.type);
+        animal.eat();
+        animal.eat(" grass");
+        animal.speak();
+        //create new animal object - cheetahobj
+        Animal cheetahObj = new Animal();
+        cheetahObj.type = "cheetah";
+        System.out.println(cheetahObj.type);
+
+
+
+    }
+}
